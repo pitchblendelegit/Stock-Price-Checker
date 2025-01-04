9852fb2658e0b4f474fbc437d3cfb1ee6f6a56ad
pitchblendelegit <72884743+pitchblendelegit@users.noreply.github.com>
2025-01-04 15:58:56 +0700

    new

--- db-connection.js
+++ db-connection.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const db = mongoose.connect(process.env.DB, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
 module.exports = db;
--- models.js
+++ models.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const stockSchema = new Schema({
-    symbol: {type: String, required: true},
-    likes: {type: [String], default: []},
+const StockSchema = new Schema({
+  symbol: { type: String, required: true },
+  likes: { type: [String], default: [] },
 });
-
-const Stock = mongoose.model("Stock", stockSchema);
+const Stock = mongoose.model("Stock", StockSchema);
 
 exports.Stock = Stock;
